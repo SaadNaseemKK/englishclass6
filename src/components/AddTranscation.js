734f65aac1462a8aa653312f3a1de631d616ca43
SaadNaseemKK <saad.naseem.462@gmail.com>
2020-06-25 16:13:59 +0500

    Update AddTranscation.js
    corrected label warning

--- src/components/AddTranscation.js
+++ src/components/AddTranscation.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
 export const AddTranscation = () => {
+
     return (
         <div>
             <h3>Add New Transcation</h3>
             <form>
                 <div className="form-control">
                     <label htmlFor="description">Description</label>
-                    <input type="text" id="description" placeholder="Income 1"/>
+                    <input type="text" id="description" placeholder="Income 1" />
                 </div>
                 <div className="form-control">
-                    <lable htmlFor="transcationamount">Transcation Amount</lable>
-                    <input type="number" id="transcationamount" placeholder="Enter amount"/>
+                    <label htmlFor="transcationamount">Transcation Amount</label>
+                    <input type="number" id="transcationamount" placeholder="Enter amount" />
                 </div>
                 <button className="btn">Add transcation</button>
             </form>
