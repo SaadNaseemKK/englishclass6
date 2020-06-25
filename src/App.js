88026865e23c4b2ec74ff5b9fa28153b99f73696
SaadNaseemKK <saad.naseem.462@gmail.com>
2020-06-25 14:27:04 +0500

    Created Components and UI
    Created Header, Balance, AccountSummary, TranscationHistroy and AddTranscation.
    
    also added Css file.
    
    also Created UI.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/Header'
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { AccountSummary } from './components/AccountSummary';
+import { TranscationHistroy } from './components/TranscationHistory';
+import { AddTranscation } from './components/AddTranscation';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <AccountSummary />
+        <TranscationHistroy />
+        <AddTranscation />
+      </div>
+
+
     </div>
   );
 }
